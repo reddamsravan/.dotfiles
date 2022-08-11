43f3ec65fd7d364ff135c723edc4097d67dce3ef
Sravan Reddam <reddamsravan@gmail.com>
2022-08-11 12:29:54 +0530

    [ZSH] Add config for NVM

--- zsh/.config/zsh/zsh-options.zsh
+++ zsh/.config/zsh/zsh-options.zsh
@@ -14,3 +14,8 @@ setopt HIST_SAVE_NO_DUPS
 # Man pages
 export MANPAGER='nvim +Man!'
 export MANWIDTH=80
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
