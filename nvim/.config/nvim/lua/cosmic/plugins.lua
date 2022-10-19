a494fbf6f2ffb46f0568048b6c50d022d45c2313
Sravan Reddam <reddamsravan@gmail.com>
2022-10-19 18:54:34 +0530

    [Plugin] add plugin for .editorconfig

--- nvim/.config/nvim/lua/cosmic/plugins.lua
+++ nvim/.config/nvim/lua/cosmic/plugins.lua
@@ -62,6 +62,8 @@ return packer.startup(function(use)
   use 'L3MON4D3/LuaSnip'
   use 'rafamadriz/friendly-snippets'
   use 'saadparwaiz1/cmp_luasnip'
+
+  use 'gpanders/editorconfig.nvim'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
