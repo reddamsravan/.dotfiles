64be9662dc18580d7f671393274d070a47a467e6
Sravan Reddam <reddamsravan@gmail.com>
2024-09-09 01:02:44 +0530

    [nvim] auto reload buffer

--- nvim/.config/nvim/lua/config/ui.lua
+++ nvim/.config/nvim/lua/config/ui.lua
@@ -1,9 +1,18 @@
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = '*',
 })
+
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+	command = "if mode() != 'c' | checktime | endif",
+	pattern = { "*" },
+})
+
+vim.api.nvim_create_autocmd({ "FileChangedShellPost" },
+	{ command = 'echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None', pattern = { "*" }, })
