a60a20d1036cb12770c5125fa8b70d7b2186f28b
Sravan Reddam <reddamsravan@gmail.com>
2024-09-09 00:56:59 +0530

    [nvim] update config

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,27 @@
+-- Space is leader
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+local opts = { noremap = true, silent = true }
+
+-- insert mode jk to go to normal mode
+vim.keymap.set('i', 'jk', '<Esc>', opts)
+
+-- save
+vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
+
+-- close
+vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
+
+
+-- Navigate between splits
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+
+--
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,38 @@
+return {
+	'echasnovski/mini.nvim',
+	version = false,
+	config = function()
+		local options = { expr = true }
+
+		-- start screen
+		require('mini.starter').setup()
+
+		-- comment
+		require('mini.comment').setup()
+
+		-- text objects
+		require('mini.ai').setup()
+
+		-- auto pairs
+		require('mini.pairs').setup()
+
+		-- find any
+		require('mini.pick').setup()
+		vim.keymap.set('n', '<leader>sf', MiniPick.builtin.files)
+		vim.keymap.set('n', '<leader>sb', MiniPick.builtin.buffers)
+		vim.keymap.set('n', '<leader>sg', MiniPick.builtin.grep_live)
+		vim.keymap.set('n', '<leader>sh', MiniPick.builtin.help)
+
+		-- surround
+		require('mini.surround').setup()
+
+		-- statusline
+		local statusline = require 'mini.statusline'
+		-- set use_icons to true if you have a Nerd Font
+		statusline.setup { use_icons = false }
+		---@diagnostic disable-next-line: duplicate-set-field
+		statusline.section_location = function()
+			return '%2l:%-2v'
+		end
+	end
+}
